d99005b07e6780608c0e765972bfb70c0b4b5a47
SimSBgit <sbct47@gmail.com>
2025-10-17 17:39:20 +0900

    oo

--- src/main/java/com/example/demo/ExCh03Application.java
+++ src/main/java/com/example/demo/ExCh03Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.example.demo", "com.example.calculator2" })
+@ComponentScan(basePackages = {"com.example.extemplate", "com.example.getpost", "com.example.demo", "com.example.calculator2" })
 public class ExCh03Application {
 
 	public static void main(String[] args) {
